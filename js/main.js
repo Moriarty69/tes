ebbe2530adf209cf00bcc1407c34e9d9ad253d38
Moriarty69 <89221607+Moriarty69@users.noreply.github.com>
2021-10-13 02:51:18 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+$(function(){
+	$('.burger_menu').click(function(){
+		$('.burger_menu').toggleClass('burger_menu_active')
+	})
+});
+
+
+
+
+
+
+/*const header = document.querySelector('.header');
+const section = document.querySelector('.section');
+
+const headerHeight = header.offsetHeight;
+const sectionHeight = section.offsetHeight;
+
+
+window.addEventListener('scroll', () => {
+
+	let	scrollDistance = window.scrollY;
+	console.log(scrollDistance);
+
+	if (scrollDistance >= sectionHeight) {
+		header.classList.add('')
+	} else{
+		header.classList.remove('')
+	}
+
+});
+*/
+
+
